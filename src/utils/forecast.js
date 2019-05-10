7a10e8083982e76ca401b07df00a8b23bf41c9a7
kwizeraelvis <kwizeraelvis6@gmail.com>
2019-05-10 19:56:30 +0200

    Forecast now Include High and Low Temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('No palce with specified coordinates', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.');
+            callback(undefined, body.daily.data[0].summary + 'Highest Temperature of the day will be ' + body.daily.data[0].temperatureHigh + ', and the Lowest temperature will hit ' + body.daily.data[0].temperatureLow +'.There is a ' + body.currently.precipProbability + '% chance of rain.');
         }
     })
 
